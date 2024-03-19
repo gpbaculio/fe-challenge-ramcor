7eedd7873faa54c0c9affb83147805391adfbdaa
Glendon Philipp Baculio <gpbaculio@gmail.com>
2024-03-20 04:56:18 +0800

    update request item

--- src/contexts/SurvivorContext.tsx
+++ src/contexts/SurvivorContext.tsx
@@ -8,11 +8,7 @@ interface SurvivorContextType {
   addSurvivor: (newSurvivor: Survivor) => void;
   listSurvivorInventory: (survivorId: string) => inventoryItemType[] | null;
   listItemsBySurvivor: (survivorId: string) => inventoryItemType[] | null;
-  requestItemFromSurvivor: (
-    requesterId: string,
-    survivorId: string,
-    itemId: string
-  ) => void;
+  requestItemFromSurvivor: (survivorId: string, itemId: string) => void;
 }
 
 const initialSurvivors: Survivor[] = [
@@ -105,30 +101,19 @@ const SurvivorProvider: React.FC<SurvivorProviderProps> = ({ children }) => {
     const survivor = survivors.find((survivor) => survivor.id === survivorId);
     return survivor ? survivor.inventory : null;
   };
-
-  const requestItemFromSurvivor = (
-    requesterId: string,
-    survivorId: string,
-    itemId: string
-  ) => {
-    const requesterIndex = survivors.findIndex(
-      (survivor) => survivor.id === requesterId
-    );
+  // descreases the quantity of the item by 1
+  const requestItemFromSurvivor = (survivorId: string, itemId: string) => {
     const survivorIndex = survivors.findIndex(
       (survivor) => survivor.id === survivorId
     );
-    if (requesterIndex !== -1 && survivorIndex !== -1) {
+    if (survivorIndex !== -1) {
       const updatedSurvivors = [...survivors];
       const itemIndex = updatedSurvivors[survivorIndex].inventory.findIndex(
         (item) => item.itemId === itemId
       );
       if (itemIndex !== -1) {
-        // Item found in survivor's inventory
-        updatedSurvivors[requesterIndex].inventory.push(
-          updatedSurvivors[survivorIndex].inventory[itemIndex]
-        );
-        // Remove item from survivor's inventory
-        updatedSurvivors[survivorIndex].inventory.splice(itemIndex, 1);
+        // Decrease the quantity of the item by 1
+        updatedSurvivors[survivorIndex].inventory[itemIndex].quantity -= 1;
         setSurvivors(updatedSurvivors);
       }
     }

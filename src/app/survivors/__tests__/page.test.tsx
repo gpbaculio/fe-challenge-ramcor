f8b472b407960f1faa32366e92f471a7b270f9df
Glendon Philipp Baculio <gpbaculio@gmail.com>
2024-03-20 12:23:51 +0800

    Update page.test.tsx

--- src/app/survivors/__tests__/page.test.tsx
+++ src/app/survivors/__tests__/page.test.tsx
@@ -1,5 +1,6 @@
 import "@testing-library/jest-dom";
 import { render } from "@testing-library/react";
+
 import SurvivorList from "../page";
 import { SurvivorProvider } from "@/contexts/SurvivorContext";
 
